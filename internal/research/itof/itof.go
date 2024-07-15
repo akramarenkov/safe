10a0a2c2495efd9c02522e22e50c4c7e3bb5d36b
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-07-15 23:40:33 +0300

    add itof research

---
+++ internal/research/itof/itof.go
@@ -0,0 +1,269 @@
+package research
+
+import (
+	"errors"
+
+	"golang.org/x/exp/constraints"
+
+	"github.com/akramarenkov/safe"
+)
+
+var (
+	ErrBeginGreaterEnd          = errors.New("begin is greater than end")
+	ErrLosslesslyLengthNegative = errors.New("losslessly length is negative")
+	ErrLosslesslyLengthTooLong  = errors.New("losslessly length is too long for specified type")
+)
+
+const (
+	maxIntPower = 19
+)
+
+// Finds last number in a continuous sequence of integers that convert to floating point
+// numbers without loss followed by a number that converts with loss.
+//
+// It is necessary to specify the required length of a continuous sequence of integers
+// that must be converted to floating point numbers without loss.
+//
+// It is also necessary to indicate the sign (search direction on the number line) of
+// the numbers for which you want to search. If the sign is negative, the search will be
+// performed for negative numbers, in other cases - for positive numbers.
+func FindLastLosslessly[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	losslesslyLength TypeFrom,
+	sign TypeFrom,
+) (TypeFrom, bool, error) {
+	if losslesslyLength < 0 {
+		return 0, false, ErrLosslesslyLengthNegative
+	}
+
+	if sign < 0 {
+		return findLastLosslesslyNegative[TypeTo](losslesslyLength, sign)
+	}
+
+	return findLastLosslesslyPositive[TypeTo](losslesslyLength, sign)
+}
+
+func findLastLosslesslyPositive[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	losslesslyLength TypeFrom,
+	sign TypeFrom,
+) (TypeFrom, bool, error) {
+	previous := TypeFrom(0)
+
+	for power := maxIntPower; power >= 0; power-- {
+		begun := false
+
+		exponent, err := safe.Pow10[TypeFrom](power)
+		if err != nil {
+			// too large a power for a given integer type, try a smaller power
+			continue
+		}
+
+		for multiplier := TypeFrom(1); multiplier <= 10; multiplier++ {
+			current, err := safe.Mul(multiplier, exponent)
+			if err != nil {
+				// too large a number for a given integer type, try a smaller power
+				break
+			}
+
+			base, err := safe.Add(previous, current)
+			if err != nil {
+				// too large a number for a given integer type, try a smaller power
+				break
+			}
+
+			number, beginning, finded, err := findLosslessly[TypeTo](
+				base,
+				losslesslyLength,
+				sign,
+			)
+			if err != nil {
+				return 0, false, err
+			}
+
+			if finded {
+				return number, true, nil
+			}
+
+			if beginning {
+				begun = true
+				continue
+			}
+
+			if begun {
+				// Since at the current step we managed to multiply the exponent and
+				// add/subtract the resulting number with a multiplier equal to
+				// 'multiplier' without overflow, then for 'multiplier-1' this will
+				// definitely be possible without overflow
+				previous += (multiplier - 1) * exponent
+				break
+			}
+		}
+	}
+
+	return 0, false, nil
+}
+
+func findLastLosslesslyNegative[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	losslesslyLength TypeFrom,
+	sign TypeFrom,
+) (TypeFrom, bool, error) {
+	previous := TypeFrom(0)
+
+	for power := maxIntPower; power >= 0; power-- {
+		begun := false
+
+		exponent, err := safe.Pow10[TypeFrom](power)
+		if err != nil {
+			// too large a power for a given integer type, try a smaller power
+			continue
+		}
+
+		for multiplier := TypeFrom(1); multiplier <= 10; multiplier++ {
+			current, err := safe.Mul(multiplier, exponent)
+			if err != nil {
+				// too large a number for a given integer type, try a smaller power
+				break
+			}
+
+			base, err := safe.Sub(previous, current)
+			if err != nil {
+				// too large a number for a given integer type, try a smaller power
+				break
+			}
+
+			number, beginning, finded, err := findLosslessly[TypeTo](
+				base,
+				losslesslyLength,
+				sign,
+			)
+			if err != nil {
+				return 0, false, err
+			}
+
+			if finded {
+				return number, true, nil
+			}
+
+			if beginning {
+				begun = true
+				continue
+			}
+
+			if begun {
+				// Since at the current step we managed to multiply the exponent and
+				// add/subtract the resulting number with a multiplier equal to
+				// 'multiplier' without overflow, then for 'multiplier-1' this will
+				// definitely be possible without overflow
+				previous -= (multiplier - 1) * exponent
+				break
+			}
+		}
+	}
+
+	return 0, false, nil
+}
+
+// Finds last number in a continuous sequence of integers that convert to floating point
+// numbers without loss followed by a number that converts with loss.
+//
+// Search range is limited by the base number and required length of a continuous
+// sequence of integers that must be converted to floating point numbers without loss.
+//
+// See svg file for visualization.
+func findLosslessly[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	base TypeFrom,
+	losslesslyLength TypeFrom,
+	sign TypeFrom,
+) (TypeFrom, bool, bool, error) {
+	if sign < 0 {
+		return findLosslesslyNegative[TypeTo](base, losslesslyLength)
+	}
+
+	return findLosslesslyPositive[TypeTo](base, losslesslyLength)
+}
+
+func findLosslesslyPositive[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	base TypeFrom,
+	losslesslyLength TypeFrom,
+) (TypeFrom, bool, bool, error) {
+	greatest, err := safe.AddT(base, losslesslyLength, 1)
+	if err != nil {
+		return 0, false, false, ErrLosslesslyLengthTooLong
+	}
+
+	for number := base; number < greatest; number++ {
+		reverted := passThroughFloat[TypeTo](number)
+
+		if reverted == number {
+			if losslesslyLength == 0 {
+				return 0, true, false, nil
+			}
+
+			losslesslyLength--
+
+			continue
+		}
+
+		if losslesslyLength == 0 {
+			return number - 1, true, true, nil
+		}
+	}
+
+	return 0, false, false, nil
+}
+
+func findLosslesslyNegative[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	base TypeFrom,
+	losslesslyLength TypeFrom,
+) (TypeFrom, bool, bool, error) {
+	leastest, err := safe.SubT(base, losslesslyLength, 1)
+	if err != nil {
+		return 0, false, false, ErrLosslesslyLengthTooLong
+	}
+
+	for number := base; number > leastest; number-- {
+		reverted := passThroughFloat[TypeTo](number)
+
+		if reverted == number {
+			if losslesslyLength == 0 {
+				return 0, true, false, nil
+			}
+
+			losslesslyLength--
+
+			continue
+		}
+
+		if losslesslyLength == 0 {
+			return number + 1, true, true, nil
+		}
+	}
+
+	return 0, false, false, nil
+}
+
+// Checks whether precision loss occurs when converting an integer to a floating point
+// number within a given range of values.
+func IsSequenceLosslessly[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	begin TypeFrom,
+	end TypeFrom,
+) (bool, error) {
+	if begin > end {
+		return false, ErrBeginGreaterEnd
+	}
+
+	for number := begin; number < end; number++ {
+		if passThroughFloat[TypeTo](number) != number {
+			return false, nil
+		}
+	}
+
+	// prevents infinite loop when end is equal max value of type
+	return passThroughFloat[TypeTo](end) == end, nil
+}
+
+// Converts an integer to a floating point number and back.
+func passThroughFloat[TypeTo constraints.Float, TypeFrom constraints.Integer](
+	number TypeFrom,
+) TypeFrom {
+	return TypeFrom(TypeTo(number))
+}
---
+++ internal/research/itof/itof_test.go
@@ -0,0 +1,196 @@
+package research
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindLastLosslessly(t *testing.T) {
+	number, finded, err := FindLastLosslessly[float32, uint64](1, 0)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, uint64(10000000000), number)
+
+	number, finded, err = FindLastLosslessly[float64, uint64](1, 0)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, uint64(10000000000000000000), number)
+
+	number, finded, err = FindLastLosslessly[float32, uint64](2, 0)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, uint64(1<<24), number)
+
+	number, finded, err = FindLastLosslessly[float64, uint64](2, 0)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, uint64(1<<53), number)
+
+	number, finded, err = FindLastLosslessly[float32, uint64](1000, 0)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, uint64(1<<24), number)
+
+	number, finded, err = FindLastLosslessly[float64, uint64](1000, 0)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, uint64(1<<53), number)
+}
+
+func TestFindLastLosslesslyNegative(t *testing.T) {
+	number, finded, err := FindLastLosslessly[float32, int64](1, -1)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, int64(-10000000000), number)
+
+	number, finded, err = FindLastLosslessly[float64, int64](1, -1)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, int64(-1000000000000000000), number)
+
+	number, finded, err = FindLastLosslessly[float32, int64](2, -1)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, int64(-1<<24), number)
+
+	number, finded, err = FindLastLosslessly[float64, int64](2, -1)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, int64(-1<<53), number)
+
+	number, finded, err = FindLastLosslessly[float32, int64](1000, -1)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, int64(-1<<24), number)
+
+	number, finded, err = FindLastLosslessly[float64, int64](1000, -1)
+	require.NoError(t, err)
+	require.True(t, finded)
+	require.Equal(t, int64(-1<<53), number)
+}
+
+func TestFindLastLosslesslyUnsuccessful(t *testing.T) {
+	_, _, err := FindLastLosslessly[float64, int8](-100, -1)
+	require.Error(t, err)
+
+	_, _, err = FindLastLosslessly[float64, uint8](100, 0)
+	require.Error(t, err)
+
+	_, _, err = FindLastLosslessly[float64, int8](100, -1)
+	require.Error(t, err)
+
+	_, finded, err := FindLastLosslessly[float64, int8](10, -1)
+	require.NoError(t, err)
+	require.False(t, finded)
+
+	_, finded, err = FindLastLosslessly[float64, uint8](10, 1)
+	require.NoError(t, err)
+	require.False(t, finded)
+}
+
+func TestIsSequenceLosslessly(t *testing.T) {
+	conclusion, err := IsSequenceLosslessly[float32](1<<24, 1<<25)
+	require.NoError(t, err)
+	require.False(t, conclusion)
+
+	conclusion, err = IsSequenceLosslessly[float64, int64](1<<53, 1<<54)
+	require.NoError(t, err)
+	require.False(t, conclusion)
+
+	conclusion, err = IsSequenceLosslessly[float32](-1<<24, 1<<24)
+	require.NoError(t, err)
+	require.True(t, conclusion)
+
+	conclusion, err = IsSequenceLosslessly[float64](math.MinInt32, math.MaxInt32)
+	require.NoError(t, err)
+	require.True(t, conclusion)
+
+	conclusion, err = IsSequenceLosslessly[float64, uint32](0, math.MaxUint32)
+	require.NoError(t, err)
+	require.True(t, conclusion)
+
+	conclusion, err = IsSequenceLosslessly[float64, int64](-1<<53, -1<<53+math.MaxUint32)
+	require.NoError(t, err)
+	require.True(t, conclusion)
+
+	conclusion, err = IsSequenceLosslessly[float64, int64](1<<53-math.MaxUint32, 1<<53)
+	require.NoError(t, err)
+	require.True(t, conclusion)
+}
+
+func TestIsSequenceLosslesslyError(t *testing.T) {
+	conclusion, err := IsSequenceLosslessly[float32](-1<<24, -1<<25)
+	require.Error(t, err)
+	require.False(t, conclusion)
+}
+
+func TestPassThroughFloat(t *testing.T) {
+	require.Equal(t, -3, passThroughFloat[float32](-3))
+	require.Equal(t, -3, passThroughFloat[float64](-3))
+	require.Equal(t, -2, passThroughFloat[float32](-2))
+	require.Equal(t, -2, passThroughFloat[float64](-2))
+	require.Equal(t, -1, passThroughFloat[float32](-1))
+	require.Equal(t, -1, passThroughFloat[float64](-1))
+	require.Equal(t, 0, passThroughFloat[float32](0))
+	require.Equal(t, 0, passThroughFloat[float64](0))
+	require.Equal(t, 1, passThroughFloat[float32](1))
+	require.Equal(t, 1, passThroughFloat[float64](1))
+	require.Equal(t, 2, passThroughFloat[float32](2))
+	require.Equal(t, 2, passThroughFloat[float64](2))
+	require.Equal(t, 3, passThroughFloat[float32](3))
+	require.Equal(t, 3, passThroughFloat[float64](3))
+
+	require.Equal(
+		t,
+		math.MinInt32,
+		passThroughFloat[float32](math.MinInt32),
+	)
+
+	// there is a loss of precision
+	require.NotEqual(
+		t,
+		math.MaxInt32,
+		passThroughFloat[float32](math.MaxInt32),
+	)
+	require.NotEqual(
+		t,
+		uint32(math.MaxUint32),
+		passThroughFloat[float32](uint32(math.MaxUint32)),
+	)
+
+	require.Equal(
+		t,
+		math.MinInt32,
+		passThroughFloat[float64](math.MinInt32),
+	)
+	require.Equal(
+		t,
+		math.MaxInt32,
+		passThroughFloat[float64](math.MaxInt32),
+	)
+	require.Equal(
+		t,
+		uint32(math.MaxUint32),
+		passThroughFloat[float64](uint32(math.MaxUint32)),
+	)
+
+	require.Equal(
+		t,
+		int64(math.MinInt64),
+		passThroughFloat[float64](int64(math.MinInt64)),
+	)
+
+	// there is a loss of precision
+	require.NotEqual(
+		t,
+		int64(math.MaxInt64),
+		passThroughFloat[float64](int64(math.MaxInt64)),
+	)
+	require.NotEqual(
+		t,
+		uint64(math.MaxUint64),
+		passThroughFloat[float64](uint64(math.MaxUint64)),
+	)
+}
