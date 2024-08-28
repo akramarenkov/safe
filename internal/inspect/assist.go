36593c1057d2c899fa480c058c66e4699f4033bb
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-08-28 19:37:25 +0300

    unify pick up range

--- internal/inspect/assist.go
+++ internal/inspect/assist.go
@@ -7,9 +7,29 @@ import (
 )
 
 // Picks up maximum and minimum values for specified type.
-func PickUpRange[Type EightBits]() (int64, int64) {
+func PickUpRange[Type UpToSixteenBits]() (int64, int64) {
 	if is.Signed[Type]() {
-		return math.MinInt8, math.MaxInt8
+		return pickUpRangeSigned[Type]()
+	}
+
+	return pickUpRangeUnsigned[Type]()
+}
+
+func pickUpRangeSigned[Type UpToSixteenBits]() (int64, int64) {
+	reference := int64(math.MaxInt16)
+
+	if int64(Type(reference)) == math.MaxInt16 {
+		return math.MinInt16, math.MaxInt16
+	}
+
+	return math.MinInt8, math.MaxInt8
+}
+
+func pickUpRangeUnsigned[Type UpToSixteenBits]() (int64, int64) {
+	reference := int64(math.MaxUint16)
+
+	if int64(Type(reference)) == math.MaxUint16 {
+		return 0, math.MaxUint16
 	}
 
 	return 0, math.MaxUint8
--- internal/inspect/assist_test.go
+++ internal/inspect/assist_test.go
@@ -15,4 +15,12 @@ func TestPickUpRange(t *testing.T) {
 	min, max = PickUpRange[uint8]()
 	require.Equal(t, int64(0), min)
 	require.Equal(t, int64(math.MaxUint8), max)
+
+	min, max = PickUpRange[int16]()
+	require.Equal(t, int64(math.MinInt16), min)
+	require.Equal(t, int64(math.MaxInt16), max)
+
+	min, max = PickUpRange[uint16]()
+	require.Equal(t, int64(0), min)
+	require.Equal(t, int64(math.MaxUint16), max)
 }
