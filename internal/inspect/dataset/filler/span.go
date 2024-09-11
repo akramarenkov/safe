ea61142839be5b89de47774c96b59bcd8a00496f
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-09-11 22:28:14 +0300

    fix span setter

--- internal/inspect/dataset/filler/span.go
+++ internal/inspect/dataset/filler/span.go
@@ -5,12 +5,8 @@ import (
 )
 
 // Returns values from begin to end inclusive.
-//
-// Allocating slices larger than 2^16 for tests is expensive.
-func Span[Type types.UpToUSI16](begin Type, end Type) []Type {
-	// The target type is obviously smaller than int type, so there can be no integer
-	// overflow here
-	span := make([]Type, 0, int(end)-int(begin)+1)
+func Span[Type types.UpToUSI32](begin Type, end Type) []Type {
+	span := make([]Type, 0, int64(end)-int64(begin)+int64(1))
 
 	for value := begin; value < end; value++ {
 		span = append(span, value)
