3a7ddc651310985221a15466cd325bbbc2a27d41
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-09-23 10:03:40 +0300

    added iter examples

---
+++ iterator_example_test.go
@@ -0,0 +1,26 @@
+package safe_test
+
+import (
+	"fmt"
+
+	"github.com/akramarenkov/safe"
+)
+
+func ExampleIter() {
+	for number := range safe.Iter[int8](126, 127) {
+		fmt.Println(number)
+	}
+
+	// Output:
+	// 126
+	// 127
+}
+
+func ExampleIterStep() {
+	for number := range safe.IterStep[int8](126, 127, 2) {
+		fmt.Println(number)
+	}
+
+	// Output:
+	// 126
+}
