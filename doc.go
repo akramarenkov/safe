222f7993847ea4ece4e665db108c8915fd3f2380
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-09-25 18:12:58 +0300

    added links

--- doc.go
+++ doc.go
@@ -1,2 +1,13 @@
 // Library that allows you to detect overflows in operations with integer numbers.
 package safe
+
+// https://go.dev/ref/spec#Numeric_types
+// https://go.dev/ref/spec#Integer_operators
+// https://go.dev/ref/spec#Integer_overflow
+
+// https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Integer-Representations.html
+// https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Unsigned-Overflow.html
+// https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Signed-Overflow.html
+
+// https://en.wikipedia.org/wiki/Signed_number_representations
+// https://en.wikipedia.org/wiki/Two's_complement
