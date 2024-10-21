26416ea6a365de9c508040b0373bc86168297f07
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-10-21 10:40:10 +0300

    extended clone benchs

--- internal/clone/clone_test.go
+++ internal/clone/clone_test.go
@@ -1,6 +1,7 @@
 package clone
 
 import (
+	"slices"
 	"testing"
 	"unsafe"
 
@@ -25,8 +26,56 @@ func Test(t *testing.T) {
 	require.NotEqual(t, expected, copied)
 }
 
-func Benchmark(b *testing.B) {
-	slice := make([]bool, 1<<6)
+func Benchmark64(b *testing.B) {
+	benchmark(b, 1<<6)
+}
+
+func BenchmarkAppend64(b *testing.B) {
+	benchmarkAppend(b, 1<<6)
+}
+
+func BenchmarkSlicesClone64(b *testing.B) {
+	benchmarkSlicesClone(b, 1<<6)
+}
+
+func Benchmark256(b *testing.B) {
+	benchmark(b, 1<<8)
+}
+
+func BenchmarkAppend256(b *testing.B) {
+	benchmarkAppend(b, 1<<8)
+}
+
+func BenchmarkSlicesClone256(b *testing.B) {
+	benchmarkSlicesClone(b, 1<<8)
+}
+
+func Benchmark512(b *testing.B) {
+	benchmark(b, 1<<9)
+}
+
+func BenchmarkAppend512(b *testing.B) {
+	benchmarkAppend(b, 1<<9)
+}
+
+func BenchmarkSlicesClone512(b *testing.B) {
+	benchmarkSlicesClone(b, 1<<9)
+}
+
+func Benchmark1024(b *testing.B) {
+	benchmark(b, 1<<10)
+}
+
+func BenchmarkAppend1024(b *testing.B) {
+	benchmarkAppend(b, 1<<10)
+}
+
+func BenchmarkSlicesClone1024(b *testing.B) {
+	benchmarkSlicesClone(b, 1<<10)
+}
+
+func benchmark(b *testing.B, size int) {
+	slice := make([]bool, size)
 
 	for range b.N {
 		slice = Slice(slice)
@@ -35,11 +84,21 @@ func Benchmark(b *testing.B) {
 	require.NotNil(b, slice)
 }
 
-func BenchmarkCloneSliceAppend(b *testing.B) {
-	slice := make([]bool, 1<<6)
+func benchmarkAppend(b *testing.B, size int) {
+	slice := make([]bool, size)
 
 	for range b.N {
 		slice = append([]bool(nil), slice...)
+	}
+
+	require.NotNil(b, slice)
+}
+
+func benchmarkSlicesClone(b *testing.B, size int) {
+	slice := make([]bool, size)
+
+	for range b.N {
+		slice = slices.Clone(slice)
 	}
 
 	require.NotNil(b, slice)
