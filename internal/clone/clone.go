7f1d7d50a68d5c2cde1d2064d31c211dfbddf6a4
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-10-22 12:20:43 +0300

    improved clone desc

--- internal/clone/clone.go
+++ internal/clone/clone.go
@@ -1,8 +1,8 @@
 // Internal package with slightly faster, for this module, cloning function.
 package clone
 
-// Creates a shallow copy of a slice via the copy function. Slightly faster (on 5-30%)
-// than append on small number of items (from 1 to 128 of type int).
+// Creates a shallow copy of a slice via the copy function. Faster on 5-30% than append
+// on the number of items from 1 to about 128 of type int.
 func Slice[Type any](slice []Type) []Type {
 	copied := make([]Type, len(slice))
 
