94f90b9bfd4ff1b6f7f485f22dc415b6cddad141
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2025-02-13 16:11:48 +0300

    rm unused err

--- internal/inspect/errors.go
+++ internal/inspect/errors.go
@@ -5,7 +5,6 @@ import "errors"
 var (
 	ErrErrorExpected         = errors.New("an error is expected but got nil")
 	ErrInspectedNotSpecified = errors.New("inspected function is not specified")
-	ErrInvalidCustomSpan     = errors.New("custom span values exceeds possible values")
 	ErrNotEqual              = errors.New("actual value is not equal to reference value")
 	ErrReferenceNotSpecified = errors.New("reference function is not specified")
 	ErrUnexpectedError       = errors.New("received unexpected error")
